3e0c060f869f4451570534c4f1b875272b87e777
kotaro-yamasaki <kotaro22856220@gmail.com>
2024-10-16 00:17:42 +0900

    roomMemberのルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\RoomMemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('rooms', RoomController::class);
+    Route::resource('roomMembers', RoomMemberController::class);
 });
 
 require __DIR__.'/auth.php';
