b58457d6df5c4cba474e8f83f65fd4a237046061
thirdlf03 <thirdlf03@gmail.com>
2024-10-18 23:46:57 +0900

    test-code

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -1,23 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
+use App\Models\Room;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Room>
+ * @extends Factory<\App\Models\Room>
  */
-class RoomFactory extends Factory
+final class RoomFactory extends Factory
 {
     /**
-     * Define the model's default state.
+     * The name of the factory's corresponding model.
      *
-     * @return array<string, mixed>
+     * @var string
+     */
+    protected $model = Room::class;
+
+    /**
+     * Define the model's default state.
      */
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'size' => fake()->randomNumber(),
+            'title' => fake()->title,
+            'data_json' => fake()->word,
+            'latitude' => fake()->optional()->randomFloat(8, -90, 90),
+            'longitude' => fake()->optional()->randomFloat(8, -180, 180),
         ];
     }
 }
