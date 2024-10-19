bccbefd239c767b8b1ebaf351b2d9591b9f7fd8b
thirdlf03 <thirdlf03@gmail.com>
2024-10-20 00:43:08 +0900

    google認証

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\FollowController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\RoomMemberController;
-use App\Http\Controllers\ChatController;
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FollowController;
 
 if (env('APP_ENV') == 'production') {
     \Illuminate\Support\Facades\URL::forceScheme('https');
 }
+
+Route::get('auth/google', [LoginController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,10 +32,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
     Route::resource('rooms', RoomController::class);
     Route::resource('roomMembers', RoomMemberController::class);
-    Route::get('/rooms',[ChatController::class,'index'])->name('chat.index');
-    Route::post('/rooms',[ChatController::class,'store'])->name('chat.store');
-    Route::get('/rooms/create',[ChatController::class,'create'])->name('chat.create');
-    Route::delete('/rooms/{room}',[ChatController::class,'destroy'])->name('chat.destroy');
+    Route::get('/rooms', [ChatController::class, 'index'])->name('chat.index');
+    Route::post('/rooms', [ChatController::class, 'store'])->name('chat.store');
+    Route::get('/rooms/create', [ChatController::class, 'create'])->name('chat.create');
+    Route::delete('/rooms/{room}', [ChatController::class, 'destroy'])->name('chat.destroy');
     //Route::resource('chats',ChatController::class);
     Route::post('/follow/{user}', [FollowController::class, 'store'])->name('follow.store');
     Route::delete('/follow/{user}', [FollowController::class, 'destroy'])->name('follow.destroy');
