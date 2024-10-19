99e723af4a3166c5a8372077559e82c5fcd394ea
kotaro-yamasaki <kotaro22856220@gmail.com>
2024-10-19 12:58:05 +0900

    chatのるーてぃんぐを設定しましたよ

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\RoomMemberController;
+use App\Http\Controllers\ChatController;
+
 use Illuminate\Support\Facades\Route;
 
 if (env('APP_ENV') == 'production') {
@@ -23,6 +25,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('rooms', RoomController::class);
     Route::resource('roomMembers', RoomMemberController::class);
+    Route::get('/rooms',[ChatController::class,'index'])->name('chat.index');
+    Route::post('/rooms',[ChatController::class,'store'])->name('chat.store');
+    Route::get('/rooms/create',[ChatController::class,'create'])->name('chat.create');
+    Route::delete('/rooms/{room}',[ChatController::class,'destroy'])->name('chat.destroy');
+    //Route::resource('chats',ChatController::class);
 });
 
 require __DIR__.'/auth.php';
