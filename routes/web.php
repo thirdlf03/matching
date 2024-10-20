619840e88a79c10ca45bdecb0948e9c5b78df5f8
Ryyyooota <r.hnmt@icloud.com>
2024-10-20 11:00:24 +0900

    検索画面・検索機能のルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/rooms/search', [RoomController::class, 'search'])->name('rooms.search');
     Route::resource('rooms', RoomController::class);
     Route::resource('roomMembers', RoomMemberController::class);
 });
