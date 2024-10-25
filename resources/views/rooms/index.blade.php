bdc8398e82ccf44e271c118e6372b55a207893a6
kotaro-yamasaki <kotaro22856220@gmail.com>
2024-10-26 02:02:49 +0900

    カレンダー機能の作成

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -119,8 +119,8 @@
                                 {{ count($room->room_members) }}</p>
                             <p class="text-black mx-7 text-sm sm:block lg:text-lg font-bold mt-4">部屋名:
                                 {{ $room->title }}</p>
-                            <p class="text-black mx-7 text-sm sm:block lg:text-lg font-bold mt-4">カテゴリー:
-                                {{ $room->category->category_name ?? 'なし' }}</p>
+                            <!-- <p class="text-black mx-7 text-sm sm:block lg:text-lg font-bold mt-4">カテゴリー:
+                                {{ $room->category->category_name ?? 'なし' }}</p> -->
                                 @if($room->date)
                             <p class="font-bold text-sm lg:text-lg mt-4">開催日:{{ $room->date }}</p> <!-- 日付の表示 -->
                             @endif
