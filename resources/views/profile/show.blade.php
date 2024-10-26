2e0134af0c5c3d4ce9e702bf9e50767fa1662734
Ryyyooota <r.hnmt@icloud.com>
2024-10-27 01:45:20 +0900

    名前の色変更

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -10,13 +10,24 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-8 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-8 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">      
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="flex px-4">
                         <div class="flex flex-col pr-8">
                             <a href="{{ route('rooms.index') }}"
                                 class="text-blue-500 hover:text-blue-700 mr-2">部屋一覧に戻る</a>
-                            <p class="text-gray-800 dark:text-gray-300 text-4xl py-4">{{ $user->name }}</p>
+                                  <p class="text-4xl py-4 
+                            @if($user->points >= 1000) 
+                                text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600
+                            @elseif($user->points >= 500) 
+                                text-red-500
+                            @elseif($user->points >= 100) 
+                                text-blue-700
+                            @else 
+                                text-gray-800 dark:text-gray-300 
+                            @endif">
+                            {{ $user->name }}
+                        </p>
                             <div class="text-gray-600 dark:text-gray-400 text-sm">
                                 <p>アカウント作成日時: {{ $user->created_at->format('Y-m-d H:i') }}</p>
                             </div>
