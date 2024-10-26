dc36276cb780f38b16f76323118747c5e140149a
kotaro-yamasaki <kotaro22856220@gmail.com>
2024-10-26 20:15:41 +0900

    UIを微修正。トップボタンの追加。

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -139,7 +139,15 @@
                     @endif
 
                     <div class="mt-2 mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg relative">
+                        
                         <div id="restored-content-{{ $room->id }}"></div>
+                        <div class="w-12 h-12 bg-grey-400 rounded-full flex items-center justify-center mr-4 mt-2">
+                                <svg class="absolute w-10 h-10 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
 
                         @if ($room->is_show == 1)
                             <p class="my-2">場所</p>
@@ -158,7 +166,7 @@
                             @endforeach
                         </ul>
                         <br>
-                        <!-- ここから投稿タグの作成 -->
+                        <!-- ここから役割タグの作成 -->
 
                         <!-- Add New Task Button and Modal -->
                         @if ($room->user_id == auth()->id())
