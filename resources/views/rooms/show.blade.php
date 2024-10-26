dc772624709cd3fbf1cfff6ea952a3052c8e4716
thirdlf03 <thirdlf03@gmail.com>
2024-10-26 17:40:44 +0900

    修正

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -44,7 +44,6 @@
                             <button @click="slideOverOpen=true; document.body.style.overflow = 'hidden';"
 
                                     class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors bg-white border rounded-md hover:bg-neutral-100 active:bg-white focus:bg-white focus:outline-none focus:ring-2 focus:ring-neutral-200/60 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none">チャット</button>
-
                             <template x-teleport="body">
                                 <div x-show="slideOverOpen"
                                      @keydown.window.escape="slideOverOpen=false; document.body.style.overflow = '';"
